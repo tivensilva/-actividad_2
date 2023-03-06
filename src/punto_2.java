d32e41347ccdecbe26de1e4aa55f91f91758aa5a
tivensilva <sarrubla@unal.edu.co>
2023-03-05 22:25:40 -0500

    Añadimos ediciones con los puntos 5 y 6

--- src/punto_2.java
+++ src/punto_2.java
@@ -3,17 +3,25 @@ import java.math.*;
 
 public class punto_2 {
     /*
-    
-     @ejercicio 3-19
+    Despúes de iniciada la clase procedemos con el esqueleto
      */
     public static void main(String[] args) {
+
+        // declaramos el tipo de datos de las variables
         double area; float perimeter; float height;
-        System.out.println("Por favor ingrese la medida del lado del tríangulo");
+
+    
+        // Iniciamos el scanner y solicitamos los 3 datos
         Scanner entryScanner = new Scanner(System.in);
+        System.out.println("Por favor ingrese la medida del lado del tríangulo");
         int side = entryScanner.nextInt();
+
+        // Realizamos los calculos
         area = (Math.sqrt(3) / 4) * Math.pow(side, 2);
         perimeter = (side + side + side);
         height = (float) Math.cbrt(side);
+
+        // Imprimimos los resultados
         System.out.println("El area del triangulo es: " + area);
         System.out.println("El perimetro del triangulo es: " + perimeter);
         System.out.println("La altura del triangulo es: " + height);
--- src/punto_3.java
+++ src/punto_3.java
@@ -12,22 +12,22 @@ public class punto_3 {
         Scanner entryScanner = new Scanner(System.in);
 
         // Definimos los tipos de variable que vamos a usar
-        float side, side2, side3, perimeter, semiperimeter, area;
+        float side_a, side_b, side_c, perimeter, semiperimeter, area;
 
         // Obtenemos los datos
-        System.out.println("Por favor ingrese la medida del primer del tríangulo");
-        side = entryScanner.nextFloat();
-        System.out.println("Por favor ingrese la medida del segundo del tríangulo");
-        side2 = entryScanner.nextFloat();
-        System.out.println("Por favor ingrese la medida del tercer del tríangulo");
-        side3 = entryScanner.nextFloat();
+        System.out.println("Por favor ingrese la medida del primer lado del triángulo tríangulo");
+        side_a = entryScanner.nextFloat();
+        System.out.println("Por favor ingrese la medida del segundo lado del tríangulo");
+        side_b = entryScanner.nextFloat();
+        System.out.println("Por favor ingrese la medida del tercer lado del tríangulo");
+        side_c = entryScanner.nextFloat();
         
         /*Acá obtenemos los dato
           lo relevante es el uso de la fórmula de Herón para calcular el área
         */ 
-        perimeter = (side + side2 + side3);
+        perimeter = (side_a + side_b + side_c);
         semiperimeter = perimeter / 2;
-        area = (float) Math.sqrt(semiperimeter * (semiperimeter - side) * (semiperimeter - side2) * (semiperimeter - side3));
+        area = (float) Math.sqrt(semiperimeter * (semiperimeter - side_a) * (semiperimeter - side_b) * (semiperimeter - side_c));
 
         // Imprimimos los resultados
         System.out.println("El area del triangulo es: " + area);
