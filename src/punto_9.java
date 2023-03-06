6cb858255e6aaeaa58a9b7d53c8aade4dac88f16
tivensilva <sarrubla@unal.edu.co>
2023-03-06 16:50:09 -0500

    Insertamos punto 8

---
+++ src/punto_9.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+/*
+    Módulo 4 - Ejercicio propuesto 14
+ */
+
+public class punto_9 {
+    public static void main(String args[]) {
+        Scanner entryScanner = new Scanner(System.in);
+    }
+}
