98150b4718b07ab5d7dd0ae5b5d66576c2d825cf
tivensilva <104739806+tivensilva@users.noreply.github.com>
2023-03-11 19:07:23 -0500

    punto 9

--- src/punto_9.java
+++ src/punto_9.java
@@ -6,6 +6,37 @@ import java.util.Scanner;
 
 public class punto_9 {
     public static void main(String args[]) {
+        // Definimos el escaner para leer datos por teclado
         Scanner entryScanner = new Scanner(System.in);
+
+        // Definimos las variables
+        int dep1, dep2, dep3, sueldo, ventas, sueldoFinal;
+        
+        // Pedimos los datos
+        System.out.print("Ingrese las ventas del departamento 1: ");
+        dep1 = entryScanner.nextInt();
+        System.out.print("Ingrese las ventas del departamento 2: ");
+        dep2 = entryScanner.nextInt();
+        System.out.print("Ingrese las ventas del departamento 3: ");
+        dep3 = entryScanner.nextInt();
+        System.out.print("Ingrese el sueldo de los vendedores: ");
+
+        // Calculamos las ventas totales
+        ventas = dep1 + dep2 + dep3;
+
+        // Calculamos los porcentajes de ventas
+        if dep1 > 0.33*ventas {
+            sueldoFinal = sueldo + 0.2*sueldo;
+        }
+        else if dep2 > 0.33*ventas {
+            sueldoFinal = sueldo + 0.2*sueldo;
+        }
+        else if dep3 > 0.33*ventas {
+            sueldoFinal = sueldo + 0.2*sueldo;
+        }
+        else {
+            sueldoFinal = sueldo;
+        }
+
     }
 }
